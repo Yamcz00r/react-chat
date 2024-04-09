f39154ae87cdf0c6dc137cde24bbc1bceff395d5
Yamcz00r <djamka@sq3-partners.com>
2024-04-09 14:53:04 +0200

    Created the routing fundament for the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './routes';
 const App = () => {
   return (
-    <h1 className="">Hello World</h1>
+    <RouterProvider router={router} />
   )
 };
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />
+    }
+]);
+
+export default router;
