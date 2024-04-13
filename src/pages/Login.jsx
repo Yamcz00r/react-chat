9ca43f628f161b099f2ea1bac2e914c1bc17cf70
Kacper <kacpersliwa644@gmail.com>
2024-04-13 10:12:37 +0200

    started making links and styling them

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
-import { Input, InputGroup, InputRightElement, Button, Stack } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement, Button, Checkbox} from '@chakra-ui/react';
 
 const Login = () => {
     return (
-        <>
-          <div style={{display:'flex', justifyContent:'center',alignItems:'center', maxHeight:'100vh',flexDirection:'column'}}>
-              <h1 style={{fontSize:'4rem'}}>Login</h1>
-              <div>
-                  <Input width='35rem' placeholder='Enter Username' size="md" />
-                  <br />
-                  <PasswordInput />
-              </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+            <h1 style={{ fontSize: '4rem', textAlign: 'center' }}>Login Page</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <Input marginTop='1.5rem' width='35rem' placeholder='Enter Username' size="md" />
+                <br />
+                <PasswordInput />
             </div>
-        </>
+            <LocalButton />
+        </div>
     )
 };
 
@@ -21,13 +20,14 @@ const PasswordInput = () => {
     const handleClick = () => setShow(!show);
 
     return (
-        <InputGroup margin-top='5rem' width='35rem' size='md'>
+        <InputGroup width='35rem' size='md'>
             <Input
                 pr='4.5rem'
                 type={show ? 'text' : 'password'}
                 placeholder='Enter password'
             />
             <InputRightElement width='4.5rem'>
+
                 <Button h='1.75rem' size='sm' onClick={handleClick}>
                     {show ? 'Hide' : 'Show'}
                 </Button>
@@ -35,14 +35,30 @@ const PasswordInput = () => {
         </InputGroup>
     );
 };
-  const LocalButton = () => {
-    
+
+const LocalButton = () => {
+    const handleClick = () => {
+        alert('Logged in!')
+    }
+    return (
+        <div style={{ marginTop: '1.5rem' }}>
+            <Button onClick={handleClick} colorScheme='blue' size='md'>Continue</Button>
+            <br />
+            <div>
+                <Checkbox colorScheme='cyan' marginTop='1.5rem' >Remember me</Checkbox>
+            </div>
+        </div>
+    )
+};
+const Links = () =>{
     return(
-      <Stack align='center'>
-        <Button onClick={handleClick} colorScheme='blue' size='sm'>Continue</Button>
-      </Stack>
-      
-  )
-  };
-export { Login, PasswordInput,LocalButton };
+        
+            <p fontSize='12px'>
+                <a href="http://">You dont have account</a>
+            </p>
+        
+    )
+};
+
+export { Login, PasswordInput, LocalButton,Links };
 export default Login;
