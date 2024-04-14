9ce9aa3ea60520ef84ecae584145764860471c37
Kacper Śliwa <kacpersliwa644@gmail.com>
2024-04-14 22:01:50 +0200

    Revert "g"
    This reverts commit ea6dff2d410c0e97191c22805ba085a3bc09f5a2.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
-import { Input, InputGroup, InputRightElement, Button, Checkbox, Box, Heading, Link } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement, Button, Checkbox} from '@chakra-ui/react';
 
 const Login = () => {
     return (
-        <Box display='flex' justifyContent='center' alignItems='center' height='100vh' flexDirection='column'>
-            <Heading fontSize='4rem' textAlign='center'>Login Page</Heading>
-            <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
-                <InputField label="Username" />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+            <h1 style={{ fontSize: '4rem', textAlign: 'center' }}>Login Page</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <Input marginTop='1.5rem' width='35rem' placeholder='Enter Username' size="md" />
                 <br />
                 <PasswordInput />
-            </Box>
+            </div>
             <LocalButton />
-            <Links />
-        </Box>
+        </div>
     )
-};
-
-const InputField = ({ label }) => {
-    return (
-        <Input marginTop='1.5rem' width='35rem' placeholder={`Enter ${label}`} size="md" />
-    );
 };
 
 const PasswordInput = () => {
@@ -32,9 +25,9 @@ const PasswordInput = () => {
                 pr='4.5rem'
                 type={show ? 'text' : 'password'}
                 placeholder='Enter password'
-                aria-label="Password"
             />
             <InputRightElement width='4.5rem'>
+
                 <Button h='1.75rem' size='sm' onClick={handleClick}>
                     {show ? 'Hide' : 'Show'}
                 </Button>
@@ -48,27 +41,24 @@ const LocalButton = () => {
         alert('Logged in!')
     }
     return (
-        <Box marginTop='1.5rem'>
+        <div style={{ marginTop: '1.5rem' }}>
             <Button onClick={handleClick} colorScheme='blue' size='md'>Continue</Button>
             <br />
-            <CheckboxField label="Remember me" />
-        </Box>
+            <div>
+                <Checkbox colorScheme='cyan' marginTop='1.5rem' >Remember me</Checkbox>
+            </div>
+        </div>
+    )
+};
+const Links = () =>{
+    return(
+        
+            <p fontSize='12px'>
+                <a href="http://">You dont have account</a>
+            </p>
+        
     )
 };
 
-const CheckboxField = ({ label }) => {
-    return (
-        <Checkbox colorScheme='cyan' marginTop='1.5rem'>{label}</Checkbox>
-    );
-};
-
-const Links = () => {
-    return (
-        <Box fontSize='12px'>
-            <Link href="http://">You don't have an account? Sign up here.</Link>
-        </Box>
-    )
-};
-
-export { Login, PasswordInput, LocalButton, Links };
+export { Login, PasswordInput, LocalButton,Links };
 export default Login;
