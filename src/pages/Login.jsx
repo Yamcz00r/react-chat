61c0a3ca5a5c0875ee3779fa55e6a5755a1fc0bf
Kacper <kacpersliwa644@gmail.com>
2024-04-15 23:12:13 +0200

    trying to make checkbox and button logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,53 @@
 import React, { useState } from 'react';
-import { Input, InputGroup, InputRightElement, Button, Checkbox, Box, Heading, Link } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement, Button, Checkbox, Box, Heading, Link, Wrap, WrapItem } from '@chakra-ui/react';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        alert('Logged in!');
+        if (rememberMe) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        }
+    };
+
+    const handleRememberMe = () => {
+        if (!rememberMe) {
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+        }
+        setRememberMe(!rememberMe);
+    };
+
     return (
-        <Box display='flex' justifyContent='center' alignItems='center' height='100vh' flexDirection='column'>
+        <Box display='flex' justifyContent='center' alignItems='center' height='100vh' flexDirection='column' marginTop='2rem'>
             <Heading fontSize='4rem' textAlign='center'>Login Page</Heading>
-            <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
-                <InputField label="Username" />
-                <br />
-                <PasswordInput />
-            </Box>
-            <LocalButton />
-            <Links />
+            <form onSubmit={handleSubmit} >
+                <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
+                    <InputField label="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <br />
+                    <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
+                </Box>
+                <LocalButton />
+                <CheckboxField label="Remember me" isChecked={rememberMe} onChange={handleRememberMe} />
+                <Links />
+            </form>
         </Box>
     )
 };
 
-const InputField = ({ label }) => {
+const InputField = ({ label, value, onChange }) => {
     return (
-        <Input marginTop='1.5rem' width='35rem' placeholder={`Enter ${label}`} size="md" />
+        <Input marginTop='1.5rem' width='35rem' placeholder={`Enter ${label}`} size="md" value={value} onChange={onChange} />
     );
 };
 
-const PasswordInput = () => {
+const PasswordInput = ({ value, onChange }) => {
     const [show, setShow] = useState(false);
     const handleClick = () => setShow(!show);
 
@@ -33,6 +58,8 @@ const PasswordInput = () => {
                 type={show ? 'text' : 'password'}
                 placeholder='Enter password'
                 aria-label="Password"
+                value={value}
+                onChange={onChange}
             />
             <InputRightElement width='4.5rem'>
                 <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -44,31 +71,53 @@ const PasswordInput = () => {
 };
 
 const LocalButton = () => {
-    const handleClick = () => {
-        alert('Logged in!')
-    }
     return (
-        <Box marginTop='1.5rem'>
-            <Button onClick={handleClick} colorScheme='blue' size='md'>Continue</Button>
-            <br />
-            <CheckboxField label="Remember me" />
+        <Box textAlign='center' marginTop='1.5rem'>
+            <Button type="submit" colorScheme='blue' size='md'>Continue</Button>
         </Box>
     )
 };
 
-const CheckboxField = ({ label }) => {
+const CheckboxField = ({ label, isChecked, onChange }) => {
     return (
-        <Checkbox colorScheme='cyan' marginTop='1.5rem'>{label}</Checkbox>
+        <Box textAlign='center'>
+            <Checkbox colorScheme='cyan' marginTop='1.5rem' isChecked={isChecked} onChange={onChange}>{label}</Checkbox>
+        </Box>
+        
     );
 };
 
 const Links = () => {
     return (
-        <Box fontSize='12px'>
-            <Link href="http://">You don't have an account? Sign up here.</Link>
-        </Box>
+        <Wrap justifyContent="center" alignItems="center" mt="6rem">
+            <WrapItem>
+                <Box fontSize='18px' marginRight="1rem">
+                    <Link href="http://">Forgot your password?</Link>
+                </Box>
+            </WrapItem>
+            <WrapItem>
+                <Box fontSize='18px' marginRight="1rem">
+                    <Link href="http://">Regulations</Link>
+                </Box>
+            </WrapItem>
+            <WrapItem>
+                <Box fontSize='18px' marginRight="1rem">
+                    <Link href="http://">Privacy Policy</Link>
+                </Box>
+            </WrapItem>
+            <WrapItem>
+                <Box fontSize='18px' marginRight="1rem">
+                    <Link href="http://">You don't have an account? Sign up here.</Link>
+                </Box>
+            </WrapItem>
+            <WrapItem>
+                <Box fontSize='18px'>
+                    <p>@Project</p>
+                </Box>
+            </WrapItem>
+        </Wrap>
     )
 };
 
-export { Login, PasswordInput, LocalButton, Links };
+export { Login, PasswordInput, LocalButton, CheckboxField, Links };
 export default Login;
