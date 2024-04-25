ea479ecf57162f42806a937013fcb89798ecaf75
Yamcz00r <dawid.jamka@onet.pl>
2024-04-25 12:25:05 +0200

    Implementeed basic messaging functioanlity (CRUD)

--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { decryptToken } from '../middleware/jwt';
-import { sendMessage } from "../services/messages";
+import { sendMessage, deleteMessage, editMessage } from "../services/messages";
 const router = Router();
 
 router.post('/send', decryptToken, sendMessage);
+router.put('/edit/:id', decryptToken, editMessage);
+router.delete('/delete/:id', decryptToken, deleteMessage);
 
 export default router;
--- server/src/services/messages.ts
+++ server/src/services/messages.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { prisma } from "../../prisma/prisma";
 
-export const sendMessage = (req: Request, res: Response) => {
+export const sendMessage = async (req: Request, res: Response) => {
     const senderId = req.user_id;
     
     const { content } = req.body;
@@ -11,14 +11,80 @@ export const sendMessage = (req: Request, res: Response) => {
         throw error;
     }
     try {
-        const result = prisma.message.create({
+        const result = await prisma.message.create({
             data: {
                 content: content,
                 userId: senderId
             }
         });
+        console.log(result)
         return res.status(200).json(result)
     } catch(error) {
+        console.error(error)
+    }
+}
+
+
+export const editMessage = async (req: Request, res: Response) => {
+    const { user_id } = req;
+    const { id } = req.params;
+    const { content } = req.body;
+    if (!content) {
+        const error = new Error("Content was not provided");
+        throw error;
+    }
+    try {
+        const message = await prisma.message.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+        if (!message) {
+            const error = new Error("You need to send the message");
+            throw error;
+        };
+        if (message.userId !== user_id) {
+            const error = new Error("You dont have permission");
+            throw error;
+        };
+        const updatedMessage = await prisma.message.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                content
+            }
+        });
+        return res.status(200).json(updatedMessage);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const deleteMessage = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user_id = req.user_id;
+    try {
+        const message = await prisma.message.findUnique({
+            where: {
+                id: Number(id),
+            }
+        });
+        if (!message) {
+            const error = new Error('No message was found!');
+            throw error;
+        }
+        if (message.userId !== user_id) {
+            const error = new Error("You dont have permission");
+            throw error;
+        };
+        const result = await prisma.message.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+        return res.status(200).json(result);
+    } catch (error) {
         console.error(error)
     }
 }
